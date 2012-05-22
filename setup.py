536f3be6ccc1dead1ed42701ac6b34bb1a24afcc
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-05-22 18:14:16 +0200

    funky building!

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from distutils.core import setup
+from numpy.distutils.core import setup as npsetup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 qpbo_dir = "../../tools/QPBO-v1.3.src/"
-files = [qpbo_dir + f for f in ["QPBO.cpp", "QPBO.cpp", "QPBO_maxflow.cpp",
-    "QPBO_postprocessing.cpp", "QPBO_extra.cpp"]]
 
-files.insert(0, "pyqpbo.pyx")
 
-setup(cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("pyqpbo", files,
-        language="c++", include_dirs=["../../tools/QPBO-v1.3.src/"])])
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('qpbo', parent_package, top_path)
+
+    # Genome module
+    sources = [qpbo_dir + f for f in ["QPBO.cpp", "QPBO.cpp",
+        "QPBO_maxflow.cpp", "QPBO_postprocessing.cpp", "QPBO_extra.cpp"]]
+    config.add_extension('libqpbo', sources=sources, language='c++')
+    return config
+
+if __name__ == '__main__':
+    npsetup(**configuration(top_path='').todict())
+
+    setup(cmdclass={'build_ext': build_ext},
+        ext_modules=[Extension("pyqpbo", ['pyqpbo.pyx'], language="c++",
+        include_dirs=[qpbo_dir], libraries=['qpbo'], library_dirs=['qpbo'])])
