cc835acb46fc30c9f42bd0a22dc38e2672fb3bfc
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-06-20 14:25:43 +0200

    Rename confusion parameters

--- pyqpbo.pyx
+++ pyqpbo.pyx
@@ -36,29 +36,29 @@ cdef extern from "QPBO.h":
 
 
 def binary_graph(np.ndarray[np.int32_t, ndim=2, mode='c'] edges,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] data_cost,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost):
-    cdef int n_nodes = data_cost.shape[0]
-    if data_cost.shape[1] != 2:
-        raise ValueError("data_cost must be of shape (n_nodes, 2).")
+        np.ndarray[np.int32_t, ndim=2, mode='c'] unary_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] pairwise_cost):
+    cdef int n_nodes = unary_cost.shape[0]
+    if unary_cost.shape[1] != 2:
+        raise ValueError("unary_cost must be of shape (n_nodes, 2).")
     if edges.shape[1] != 2:
-        raise ValueError("data_cost must be of shape (n_edges, 2).")
-    if smoothness_cost.shape[0] != smoothness_cost.shape[1]:
-        raise ValueError("smoothness_cost must be square matrix.")
+        raise ValueError("unary_cost must be of shape (n_edges, 2).")
+    if pairwise_cost.shape[0] != pairwise_cost.shape[1]:
+        raise ValueError("pairwise_cost must be square matrix.")
     cdef int n_edges = edges.shape[0] 
     # create qpbo object
     cdef QPBO[int] * q = new QPBO[int](n_nodes, n_edges)
     q.AddNode(n_nodes)
-    cdef int* data_ptr = <int*> data_cost.data
+    cdef int* data_ptr = <int*> unary_cost.data
     # add unary terms
     for i in xrange(n_nodes):
         q.AddUnaryTerm(i, data_ptr[2 * i], data_ptr[2 * i + 1])
     # add pairwise terms
     # we have global terms
-    cdef int e00 = smoothness_cost[0, 0]
-    cdef int e10 = smoothness_cost[1, 0]
-    cdef int e01 = smoothness_cost[0, 1]
-    cdef int e11 = smoothness_cost[1, 1]
+    cdef int e00 = pairwise_cost[0, 0]
+    cdef int e10 = pairwise_cost[1, 0]
+    cdef int e01 = pairwise_cost[0, 1]
+    cdef int e11 = pairwise_cost[1, 1]
 
     for e in edges:
         q.AddPairwiseTerm(e[0], e[1], e00, e10, e01, e11)
@@ -78,29 +78,29 @@ def binary_graph(np.ndarray[np.int32_t, ndim=2, mode='c'] edges,
     return result
 
 
-def binary_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost, verbose=0):
-    cdef int h = data_cost.shape[0]
-    cdef int w = data_cost.shape[1]
+def binary_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] unary_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] pairwise_cost, verbose=0):
+    cdef int h = unary_cost.shape[0]
+    cdef int w = unary_cost.shape[1]
     if verbose > 0:
         print("w: %d, h: %d" % (w, h))
-    if data_cost.shape[2] != 2:
-        raise ValueError("data_cost must be of shape (h, w, 2).")
+    if unary_cost.shape[2] != 2:
+        raise ValueError("unary_cost must be of shape (h, w, 2).")
     cdef int n_nodes = w * h
     cdef int n_edges = (w - 1) * h + (h - 1) * w
     # create qpbo object
     cdef QPBO[int] * q = new QPBO[int](n_nodes, n_edges)
     q.AddNode(n_nodes)
-    cdef int* data_ptr = <int*> data_cost.data
+    cdef int* data_ptr = <int*> unary_cost.data
     # add unary terms
     for i in xrange(n_nodes):
         q.AddUnaryTerm(i, data_ptr[2 * i], data_ptr[2 * i + 1])
     # add pairwise terms
     # we have global terms
-    cdef int e00 = smoothness_cost[0, 0]
-    cdef int e10 = smoothness_cost[1, 0]
-    cdef int e01 = smoothness_cost[0, 1]
-    cdef int e11 = smoothness_cost[1, 1]
+    cdef int e00 = pairwise_cost[0, 0]
+    cdef int e10 = pairwise_cost[1, 0]
+    cdef int e01 = pairwise_cost[0, 1]
+    cdef int e11 = pairwise_cost[1, 1]
 
     for i in xrange(h):
         for j in xrange(w):
@@ -128,16 +128,16 @@ def binary_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
     return result
 
 
-def binary_grid_VH(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost,
+def binary_grid_VH(np.ndarray[np.int32_t, ndim=3, mode='c'] unary_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] pairwise_cost,
         np.ndarray[np.int32_t, ndim=2, mode='c'] V,
         np.ndarray[np.int32_t, ndim=2, mode='c'] H):
 
-    cdef int h = data_cost.shape[0]
-    cdef int w = data_cost.shape[1]
+    cdef int h = unary_cost.shape[0]
+    cdef int w = unary_cost.shape[1]
     print("w: %d, h: %d" % (w, h))
-    if data_cost.shape[2] != 2:
-        raise ValueError("data_cost must be of shape (h, w, 2).")
+    if unary_cost.shape[2] != 2:
+        raise ValueError("unary_cost must be of shape (h, w, 2).")
     if V.shape[0] != h - 1 or V.shape[1] != w:
         raise ValueError("V must be of shape (h-1, w), got (%d, %d)." % (V.shape[0], V.shape[1]))
     if H.shape[0] != h or H.shape[1] != w - 1:
@@ -148,16 +148,16 @@ def binary_grid_VH(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
     # create qpbo object
     cdef QPBO[int] * q = new QPBO[int](n_nodes, n_edges)
     q.AddNode(n_nodes)
-    cdef int* data_ptr = <int*> data_cost.data
+    cdef int* data_ptr = <int*> unary_cost.data
     # add unary terms
     for i in xrange(n_nodes):
         q.AddUnaryTerm(i, data_ptr[2 * i], data_ptr[2 * i + 1])
     # add pairwise terms
     # we have global terms
-    cdef int e00 = smoothness_cost[0, 0]
-    cdef int e10 = smoothness_cost[1, 0]
-    cdef int e01 = smoothness_cost[0, 1]
-    cdef int e11 = smoothness_cost[1, 1]
+    cdef int e00 = pairwise_cost[0, 0]
+    cdef int e10 = pairwise_cost[1, 0]
+    cdef int e01 = pairwise_cost[0, 1]
+    cdef int e11 = pairwise_cost[1, 1]
     cdef int vert_cost
     cdef int horz_cost
 
@@ -188,11 +188,11 @@ def binary_grid_VH(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
     return result
 
 
-def alpha_expansion_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost, int n_iter=3, verbose=0, random_seed=None):
-    cdef int h = data_cost.shape[0]
-    cdef int w = data_cost.shape[1]
-    cdef int n_labels =  data_cost.shape[2]
+def alpha_expansion_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] unary_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] pairwise_cost, int n_iter=3, verbose=0, random_seed=None):
+    cdef int h = unary_cost.shape[0]
+    cdef int w = unary_cost.shape[1]
+    cdef int n_labels =  unary_cost.shape[2]
     cdef int n_edges = (w - 1) * h + (h - 1) * w
     cdef int n_nodes = w * h
     cdef np.ndarray[np.int32_t, ndim=2] x
@@ -218,12 +218,12 @@ def alpha_expansion_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
     cdef int* x_ptr = <int*> x.data
     cdef int* x_ptr_current
 
-    cdef int* data_ptr = <int*> data_cost.data
+    cdef int* data_ptr = <int*> unary_cost.data
     cdef int* data_ptr_current
 
     # create qpbo object
     cdef QPBO[int] * q = new QPBO[int](n_nodes, n_edges)
-    #cdef int* data_ptr = <int*> data_cost.data
+    #cdef int* data_ptr = <int*> unary_cost.data
     for n in xrange(n_iter):
         if verbose > 0:
             print("iteration: %d" % n)
@@ -242,18 +242,18 @@ def alpha_expansion_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
                         q.AddUnaryTerm(node_id, data_ptr_current[node_label], 100000)
                     else:
                         q.AddUnaryTerm(node_id, data_ptr_current[node_label], data_ptr_current[alpha])
-                    e01 = smoothness_cost[node_label, alpha]
-                    e11 = smoothness_cost[alpha, alpha]
+                    e01 = pairwise_cost[node_label, alpha]
+                    e11 = pairwise_cost[alpha, alpha]
                     if i < h - 1:
                         #down
-                        e00 = smoothness_cost[node_label, x_ptr_current[w]]
-                        e10 = smoothness_cost[alpha, x_ptr_current[w]]
+                        e00 = pairwise_cost[node_label, x_ptr_current[w]]
+                        e10 = pairwise_cost[alpha, x_ptr_current[w]]
 
                         q.AddPairwiseTerm(node_id, node_id + w, e00, e01, e10, e11)
                     if j < w - 1:
                         #right
-                        e00 = smoothness_cost[node_label, x_ptr_current[1]]
-                        e10 = smoothness_cost[alpha, x_ptr_current[1]]
+                        e00 = pairwise_cost[node_label, x_ptr_current[1]]
+                        e10 = pairwise_cost[alpha, x_ptr_current[1]]
                         q.AddPairwiseTerm(node_id, node_id + 1, e00, e01, e10, e11)
             q.Solve()
             q.ComputeWeakPersistencies()
@@ -279,11 +279,11 @@ def alpha_expansion_grid(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
 
 
 def alpha_expansion_graph(np.ndarray[np.int32_t, ndim=2, mode='c'] edges,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] data_cost,
-        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost, int n_iter=5, verbose=False, random_seed=None):
+        np.ndarray[np.int32_t, ndim=2, mode='c'] unary_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] pairwise_cost, int n_iter=5, verbose=False, random_seed=None):
 
-    cdef int n_nodes = data_cost.shape[0]
-    cdef int n_labels =  data_cost.shape[1]
+    cdef int n_nodes = unary_cost.shape[0]
+    cdef int n_labels =  unary_cost.shape[1]
     cdef int n_edges = edges.shape[0]
     cdef np.ndarray[np.int32_t, ndim=1] x
     cdef int old_label
@@ -305,12 +305,12 @@ def alpha_expansion_graph(np.ndarray[np.int32_t, ndim=2, mode='c'] edges,
     cdef int* x_ptr = <int*> x.data
     cdef int* x_ptr_current
 
-    cdef int* data_ptr = <int*> data_cost.data
+    cdef int* data_ptr = <int*> unary_cost.data
     cdef int* data_ptr_current
 
     # create qpbo object
     cdef QPBO[int] * q = new QPBO[int](n_nodes, n_edges)
-    #cdef int* data_ptr = <int*> data_cost.data
+    #cdef int* data_ptr = <int*> unary_cost.data
     for n in xrange(n_iter):
         if verbose > 0:
             print("iteration: %d" % n)
@@ -321,17 +321,17 @@ def alpha_expansion_graph(np.ndarray[np.int32_t, ndim=2, mode='c'] edges,
                 # first state is "keep x", second is "switch to alpha"
                 # TODO: what if state is already alpha? Need to collapse?
                 if alpha == x[i]:
-                    q.AddUnaryTerm(i, data_cost[i, x_ptr[i]], 100000)
+                    q.AddUnaryTerm(i, unary_cost[i, x_ptr[i]], 100000)
                 else:
-                    q.AddUnaryTerm(i, data_cost[i, x_ptr[i]], data_cost[i, alpha])
+                    q.AddUnaryTerm(i, unary_cost[i, x_ptr[i]], unary_cost[i, alpha])
             for e in xrange(n_edges):
                 edge0 = edge_ptr[2 * e]
                 edge1 = edge_ptr[2 * e + 1]
                 #down
-                e00 = smoothness_cost[x_ptr[edge0], x_ptr[edge1]]
-                e01 = smoothness_cost[x_ptr[edge0], alpha]
-                e10 = smoothness_cost[alpha, x_ptr[edge1]]
-                e11 = smoothness_cost[alpha, alpha]
+                e00 = pairwise_cost[x_ptr[edge0], x_ptr[edge1]]
+                e01 = pairwise_cost[x_ptr[edge0], alpha]
+                e10 = pairwise_cost[alpha, x_ptr[edge1]]
+                e11 = pairwise_cost[alpha, alpha]
                 q.AddPairwiseTerm(edge0, edge1, e00, e01, e10, e11)
 
             q.Solve()
