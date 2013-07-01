69dafe09da56b9468227af6784a030d62291e4d8
Andy <amueller@ais.uni-bonn.de>
2013-07-01 17:27:38 +0200

    use setuptools not distutils...

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import tarfile
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
@@ -22,7 +22,7 @@ files = [os.path.join(qpbo_directory, f) for f in files]
 files.insert(0, "pyqpbo.pyx")
 
 setup(cmdclass={'build_ext': build_ext},
-      requires=['Cython'],
+      install_requires=['Cython'],
       ext_modules=[Extension("pyqpbo", files, language="c++",
                              include_dirs=[qpbo_directory, np.get_include()],
                              library_dirs=[qpbo_directory],
