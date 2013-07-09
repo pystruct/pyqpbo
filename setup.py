e71bf8e5957d07134058795ef41f1bd7b2bdb5f7
Forest Gregg <fgregg@uchicago.edu>
2013-07-09 09:24:40 -0500

    remove unnecessary lines

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import os
 import tarfile
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 import numpy as np
 import urllib
 
@@ -14,15 +13,6 @@ tfile = tarfile.open("QPBO-v1.3.src.tar.gz", 'r:gz')
 tfile.extractall('.')
 
 qpbo_directory = "QPBO-v1.3.src"
-
-extensions = Extension('pyqpbo', 
-                       ['pyqpbo.pyx'], 
-                       language='c++', 
-                       include_dirs=[qpbo_directory, np.get_include()],
-                       library_dirs=[qpbo_directory],
-                       extra_compile_args=["-fpermissive"])
-
-#setup(ext_modules = cythonize([extensions]))
 
 setup(ext_modules = [Extension('pyqpbo', 
                                ['pyqpbo.cpp'],
